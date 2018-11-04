ed28cadbee4d32029b67dba73836b64875390149
wojcptak <wojciech.ptak@live.com>
2018-11-04 20:06:51 +0100

    favicon errors fix

--- src/main/java/beans/springmvc/controllers/MainController.java
+++ src/main/java/beans/springmvc/controllers/MainController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -13,7 +14,16 @@ public class MainController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home() {
-        return "redirect:/users";
+        return "redirect:/index";
     }
 
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "favicon.ico", method = RequestMethod.GET)
+    @ResponseBody
+    void favicon() {}
 }
